f3cd75f0594f6a58d9522741fdc5e334263e1b90
VishalSingh79 <us55547660@gmail.com>
2025-04-11 17:50:39 +0530

    issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import Login from './components/login'
+import Login from './components/Login'
 import Navbar from './components/Navbar'
 import Products from './components/Products'
 import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
